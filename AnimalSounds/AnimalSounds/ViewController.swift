c60653a98b6cb421b1a44a719cec81e39e3bbd0a
ayns <peenats1203@gmail.com>
2019-04-19 01:56:14 -0700

    Add Animal Sound

--- AnimalSounds/AnimalSounds/ViewController.swift
+++ AnimalSounds/AnimalSounds/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var animalSoundLabel: UILabel!
+    
+    let meowSound = SimpleSound(named: "meow")
+    let woofSound = SimpleSound(named: "woof")
+    let mooSound = SimpleSound(named: "moo")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    
+    @IBAction func catButtTapped(_ sender: Any) {
+        animalSoundLabel.text = "Meow"
+        meowSound.play()
+    }
+    
+    
+    @IBAction func dogButtTapped(_ sender: Any) {
+        animalSoundLabel.text = "Woof"
+        woofSound.play()
+    }
+    
+    
+    @IBAction func cawButtTapped(_ sender: Any) {
+        animalSoundLabel.text = "Moo"
+        mooSound.play()
+    }
 }
 
